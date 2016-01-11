e1b2b3e66c8e93fd1bc173ba512c28783b91bea7
Chris F Ravenscroft <github@voilaweb.com>
2016-01-11 02:48:33 +0000

    Minor annotations

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 A port of https://github.com/soveran/micromachine
 
+Lightly annotated where Ruby idioms were converted to Crystal.
+
 ## Contributing
 
-1. Fork it ( https://github.com/[your-github-name]/MicroMachine/fork )
+1. Fork it ( https://github.com/fusion/MicroMachine/fork )
 2. Create your feature branch (git checkout -b my-new-feature)
 3. Commit your changes (git commit -am 'Add some feature')
 4. Push to the branch (git push origin my-new-feature)
--- src/MicroMachine.cr
+++ src/MicroMachine.cr
@@ -9,15 +9,22 @@ module MicroMachine
 
   class MicroMachine
 
+    # Properties v. Attributes: getter replaces _attr_reader
     getter transitions_for, state
 
     def initialize(initial_state)
       @state = initial_state
+      # In Crystal, Duck typing works to a certain point.
+      # This can be originally irritating, until you realize that forcing
+      # us to specify types means that most type confusions will cause the
+      # program to fail at compile time rather than in production.
       @transitions_for = {} of Symbol => Hash(Symbol, Symbol)
       @callbacks = Hash(Symbol, Array(Proc(Void))).new { [] of Proc(Void) }
     end
 
     def on(key, &block)
+      # Added this default initialization as the block passed in the
+      # constructor would not be invoked upon a "set" operation
       if !@callbacks.has_key?(key)
         @callbacks[key] = @callbacks[key]
       end
@@ -33,6 +40,8 @@ module MicroMachine
     end
 
     def trigger!(event)
+      # Ruby allows you to use "and" and "or" as you would in Bash, to mark
+      # success/failure conditions. Using "if" and "unless" instead.
       raise InvalidState.new("Event '#{event}' not valid from state '#{@state}'") unless
           trigger(event)
     end
@@ -47,12 +56,18 @@ module MicroMachine
     end
 
     def states
+      # Note how we can also invoke a mapped function. In Ruby, we would use
+      # "&:" as long as our object responds to to_proc
+      # In Crystal, we use "&." which is syntactic sugar for an actual block.
       transitions_for.values.map(&.to_a).flatten.uniq
     end
 
     private def change(event)
       @state = transitions_for[event][@state]
       callbacks = @callbacks[@state] + @callbacks[:any]
+      # The Ruby code would not compile "as is" as it did not seem to know
+      # whether to expect a parameter (event) or not. Opting for the latter,
+      # which seem cleaner in a state machine.
       callbacks.each { |callback| callback.call }
       true
     end
